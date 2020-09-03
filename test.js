608cd4002cd9bf86f9c7e9d3687a18c78ab9c52c
AnhTuanIT <phamanhtuan24031999@gmail.com>
2020-09-03 17:40:21 +0700

    fix do task every hour

--- test.js
+++ test.js
@@ -1,7 +1,16 @@
-const passwd = 'u1h1u1s1o1a1c';
+const { format } = require('date-fns');
+const { CronJob } = require('cron');
 
-const hass = (passwd) => {
-    console.log(passwd.split('1').reverse().join(''));
-};
+// let job = new CronJob('* * 1 * * *', () => {
+//     console.log(format(new Date(), 'Du doan 1 tieng ' + 'hh:mm (dd/MM/yyyy)'));
+// }, null, true, 'Asia/Ho_Chi_Minh');
+// job.start();
 
-hass(passwd);
+let task = new CronJob('1 * * * *', () => {
+    console.log(format(new Date(), 'Du doan 1 phut ' + 'hh:mm (dd/MM/yyyy)'));
+}, null, true, 'Asia/Ho_Chi_Minh');
+task.start();
+
+setInterval(() => {
+    console.log('Du doan 1 phut ' + format(new Date(), 'hh:mm (dd/MM/yyyy)'));
+}, 1000);
